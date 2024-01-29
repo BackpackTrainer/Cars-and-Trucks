e492652b68382c502bfb3a900d9b4b946086561b
BackpackTrainer <bill@fairfieldco.com>
2024-01-29 10:57:14 -0500

    Added chrome option to get Chrome working

--- src/test/java/stepdefinitions/NavigationStepDefinitions.java
+++ src/test/java/stepdefinitions/NavigationStepDefinitions.java
@@ -1,14 +1,11 @@
 package stepdefinitions;
 
 import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,25 +21,15 @@ public class NavigationStepDefinitions {
 //        WebDriverManager.firefoxdriver().setup();
 //        driver = new FirefoxDriver();
 
-//        WebDriverManager.chromedriver().driverVersion("121.0.6167.86").setup();
-//        driver = new ChromeDriver();
-
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-        driver = new ChromeDriver();
-
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-extensions"); // Disabling extensions
-//        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource problems
-//        options.setExperimentalOption("useAutomationExtension", false);
-//        options.addArguments("--no-sandbox"); // Bypass OS security model
-//
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver(options);
-
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
     }
 
     @Given("I have a browser open")
     public void iHaveABrowserOpen() {
+
     }
 
     @When("I navigate to {string}")
@@ -52,7 +39,6 @@ public class NavigationStepDefinitions {
 
     @Then("I see the {string} page")
     public void iSeeThePage(String pageTitle) {
-
         String tabTitle = driver.getTitle();
         assertTrue(tabTitle.contains(pageTitle));
     }
--- src/test/resources/features/SimpleNavigation.feature
+++ src/test/resources/features/SimpleNavigation.feature
@@ -1,8 +1,17 @@
-  Feature:  Simple Navigation
-
-
+Feature:  Simple Navigation
 
   Scenario:  Navigating to Barclays
     Given I have a browser open
     When  I navigate to "https://home.barclays/"
     Then I see the "Barclays" page
+
+
+  Scenario Outline: More navigation tests
+    Given I have a browser open
+    When  I navigate to <url_address>
+    Then I see the <page_title> page
+
+    Examples:
+      | url_address                  | page_title  |
+      | "http://www.fairfieldco.com" | "Fairfield" |
+      | "http://www.google.com"      | "Google"    |
